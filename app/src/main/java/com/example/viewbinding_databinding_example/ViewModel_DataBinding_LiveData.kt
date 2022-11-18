ac5fa35a5cb274a677dcc78c4075a89cb864de47
zqudwnsz@naver.com <zqudwnsz@naver.com>
2022-11-18 21:32:12 +0900

    Add: DataBiding + ViewModel + LiveData 에서의 ViewModel 추가

---
+++ app/src/main/java/com/example/viewbinding_databinding_example/ViewModel_DataBinding_LiveData.kt
@@ -0,0 +1,20 @@
+package com.example.viewbinding_databinding_example
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ViewModel_DataBinding_LiveData : ViewModel() {
+    private var number = MutableLiveData<Int>()
+
+    init {
+        number.value = 0
+    }
+
+    fun increase() {
+        number.value = number.value?.plus(1)
+    }
+    fun getNumber() : MutableLiveData<Int> {
+        return number
+    }
+
+}
