1d231197fc9708de8d659c82b014a7310767d325
zqudwnsz@naver.com <zqudwnsz@naver.com>
2022-11-18 21:33:51 +0900

    Add: ViewBinding과 DataBinding을 보여주는 액티비티

--- app/src/main/java/com/example/viewbinding_databinding_example/MainActivity.kt
+++ app/src/main/java/com/example/viewbinding_databinding_example/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.viewbinding_databinding_example
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.viewbinding_databinding_example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnViewbinding.setOnClickListener {
+            startActivity(Intent(this@MainActivity,ViewBinding::class.java))
+        }
+        binding.btnViewbindingViewmodel.setOnClickListener {
+            startActivity(Intent(this@MainActivity,ViewBinding_ViewModel::class.java))
+        }
+        binding.btnViewbindingViewmodelLivedata.setOnClickListener {
+            startActivity(Intent(this@MainActivity,ViewBinding_ViewModel_LiveData::class.java))
+        }
+        binding.btnDatabiding.setOnClickListener {
+            startActivity(Intent(this@MainActivity,DataBinding::class.java))
+        }
+        binding.btnDatabidingViewmodel.setOnClickListener {
+            startActivity(Intent(this@MainActivity,DataBinding_ViewModel::class.java))
+        }
+        binding.btnDatabidingViewmodelLivedata.setOnClickListener {
+            startActivity(Intent(this@MainActivity,DataBinding_ViewModel_LiveData::class.java))
+        }
     }
 }
